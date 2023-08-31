8b487974b3b0d24782002f2f306461d4b4ff75fe
Samiullah.Amanzai <Samiullah.Amanzai1361@gmail.com>
2023-08-31 09:42:45 -0400

    login page update

---
+++ src/SignUpPage.java
@@ -0,0 +1,11 @@
+
+public class SignUpPage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello");
+		System.out.println("what is your name");
+	}
+
+}
---
+++ src/loginPage.java
@@ -0,0 +1,14 @@
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is loging page code");
+		
+		
+		
+
+
+	}
+
+}
